9c1cef1bcb0234d83078b85760775bbd529aaf02
thiri3377 <thirihsumyataung@aol.com>
2020-04-26 06:45:44 -0400

    change

--- app.js
+++ app.js
@@ -69,6 +69,6 @@ app.post("/", function(req,res){
 app.post("./failure", function (req,res){ 
     res.redirect("/"); 
 }); 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
