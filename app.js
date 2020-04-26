8cd4021e3eb85c6a4252ad65a2fa16ae8050ba5f
thiri3377 <thirihsumyataung@aol.com>
2020-04-26 05:42:33 -0400

    change my port

--- app.js
+++ app.js
@@ -69,6 +69,6 @@ app.post("/", function(req,res){
 app.post("./failure", function (req,res){ 
     res.redirect("/"); 
 }); 
-app.listen(process.env.PORT || 3000, function () { 
-console.log(" Server is running on port 3000"); 
-}); 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
